f6de6b51c1bcd7477fb93467941ddd62872ebfc7
Giles Papworth <giles@superchiefsoftware.com>
2020-11-19 09:46:35 +0000

    Initial commit

---
+++ source/Kyameru.Component.Ftp/ConfigSetup.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Kyameru.Component.Ftp
+{
+    /// <summary>
+    /// Configuration extensions.
+    /// </summary>
+    public static class ConfigSetup
+    {
+        /// <summary>
+        /// Valid from headers
+        /// </summary>
+        private static string[] fromHeaders = new string[] { "Target", "Host", "UserName", "Password", "Recursive", "PollTime", "Delete", "Port" };
+
+        /// <summary>
+        /// Converts incoming headers to valid processing headers.
+        /// </summary>
+        /// <param name="incoming">Incoming dictionary.</param>
+        /// <returns>Returns a dictionary of valid headers.</returns>
+        public static Dictionary<string, string> ToFromConfig(this Dictionary<string, string> incoming)
+        {
+            Dictionary<string, string> response = new Dictionary<string, string>();
+            for (int i = 0; i < fromHeaders.Length; i++)
+            {
+                if (incoming.ContainsKey(fromHeaders[i]))
+                {
+                    response.Add(fromHeaders[i], incoming[fromHeaders[i]]);
+                }
+            }
+
+            return response;
+        }
+    }
+}
---
+++ source/Kyameru.Component.Ftp/Inflator.cs
@@ -0,0 +1,20 @@
+﻿using Kyameru.Core.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Kyameru.Component.Ftp
+{
+    public class Inflator : IOasis
+    {
+        public IFromComponent CreateFromComponent(Dictionary<string, string> headers)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IToComponent CreateToComponent(Dictionary<string, string> headers)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
